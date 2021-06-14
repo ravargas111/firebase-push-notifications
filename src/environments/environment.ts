faac7a7e20fd74015ac68f288fa1b7d142e33fbf
Rodrigo Vargas Abarca <42876568+ravargas111@users.noreply.github.com>
2021-06-13 22:52:37 -0600

    Clean default firebase values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   configFirebase : {
-    apiKey: "AIzaSyCHJaaelphxBH8IKttxCVbTHrobYRMG4BM",
-    authDomain: "ejemplo-progra-web.firebaseapp.com",
-    projectId: "ejemplo-progra-web",
-    storageBucket: "ejemplo-progra-web.appspot.com",
-    messagingSenderId: "890056501866",
-    appId: "1:890056501866:web:29c5bd5d1d2f8e9731b813"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
 };
 
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts("https://www.gstatic.com/firebasejs/8.6.7/firebase-messaging.js");
 
 firebase.initializeApp(
     {
-        apiKey: "AIzaSyCHJaaelphxBH8IKttxCVbTHrobYRMG4BM",
-        authDomain: "ejemplo-progra-web.firebaseapp.com",
-        projectId: "ejemplo-progra-web",
-        storageBucket: "ejemplo-progra-web.appspot.com",
-        messagingSenderId: "890056501866",
-        appId: "1:890056501866:web:29c5bd5d1d2f8e9731b813"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
       }
 );
 
